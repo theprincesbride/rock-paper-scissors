7ce90057f136594c35a1530a960e25991c3e62b1
theprincesbride <140436808+theprincesbride@users.noreply.github.com>
2023-09-26 15:32:35 -0400

    Add playRound function
    Added a working playRound function with every possible outcome of rock,
    paper, scissors against the computer opponent.  At first, I had the
    outcome show as an alert, but I changed this to return a string per the
    instructions in The Odin Project assignment.
    
     Changes to be committed:
    	modified:   external.js

--- external.js
+++ external.js
@@ -7,3 +7,22 @@ function getComputerChoice() {
 let playerSelection = prompt("Please choose rock, paper or scissors.", "").toLowerCase();
 let computerSelection = getComputerChoice();
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') 
+    {return 'Congratulations, your opponent chose scissors.  You win!';}
+    else if (playerSelection == 'rock' && computerSelection == 'paper')
+    {return 'I\'m sorry, your opponent chose paper. You lose!';}
+    else if (playerSelection == 'rock' && computerSelection == 'rock')
+    {return 'Your opponent also chose rock. It\'s a tie!';}
+    else if (playerSelection == 'scissors' && computerSelection == 'paper')
+    {return 'Congratulations, your opponent chose paper.  You win!';}
+    else if (playerSelection == 'scissors' && computerSelection == 'rock')
+    {return 'I\'m sorry, your opponent chose rock. You lose!';}
+    else if (playerSelection == 'scissors' && computerSelection == 'scissors')
+    {return 'Your opponet also chose scissors. It\'s a tie!'}
+    else if (playerSelection == 'paper' && computerSelection == 'rock')
+    {return 'Congratulations, your opponent chose paper. You win!';}
+    else if (playerSelection == 'paper' && computerSelection == 'scissors')
+    {return 'I\'m sorry, your opponent chose scissors. You lose!';}
+    else {return 'Your opponent also chose paper. It\'s a tie!'} 
+}
